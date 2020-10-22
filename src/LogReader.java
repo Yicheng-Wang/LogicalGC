0d438493e7040817d0db428896b4bd8c5c06b998
Yicheng <wangyicheng215@mails.ucas.ac.cn>
2020-10-22 15:07:43 +0800

    Read log

---
+++ src/LogReader.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class LogReader {
+    public static String[] LoadLog(String logPath) throws  IOException{
+        File logFile = new File(logPath);
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(logFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("文件不存在或者文件不可读或者文件是目录");
+        }
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+
+        while ((len = fileInputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, len);
+        }
+
+        byte[] data = outputStream.toByteArray();
+        fileInputStream.close();
+        String content = new String(data);
+        String[] rows = content.split("\r\n");
+        return rows;
+    }
+
+    public static void main(String[] args) throws IOException {
+        //String logPath = args[0];
+        String logPath = "C:\\Users\\DELL\\Desktop\\gc.log";
+        String[] rows = LoadLog(logPath);
+    }
+}
